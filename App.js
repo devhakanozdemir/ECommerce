4091326c58df47a78ce53205a2fc69340cff8a11
devhakanozdemir <dev.hakanozdemir@gmail.com>
2023-09-05 15:55:20 +0300

    create HomePage and ProductDetail screens. import axios and navigation

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './screen/HomePage';
+import ProductDetail from './screen/ProductDetail';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='HomePage' component={HomePage} />
+        <Stack.Screen name='ProductDetail' component={ProductDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,3 +29,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
